836ed9586460b0ed7a088a784b98cc4e8ec67d6e
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-03-30 23:28:06 +0530

    created backend
    Created backend & only import express() & run the backend on a port

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,9 +1,20 @@
 import "./TodoForm.css";
+import { useState } from "react";
 
 const TodoForm = () => {
+  const [text, setText] = useState("");
+  const onFormSubmit = () => {};
+  const onInputChange = (e) => {
+    //console.log(e.target.value);
+    setText(e.target.value);
+  };
   return (
-    <form>
-      <input placeholder="  Enter new task into your to do list..."></input>
+    <form className="form" onSubmit={onFormSubmit}>
+      <input
+        className="input"
+        placeholder="  Enter new task into your to do list..."
+        onChange={onInputChange}
+      ></input>
     </form>
   );
 };
