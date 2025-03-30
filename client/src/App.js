a5cbeb59950e4879e2a10da332d35a87c4b47a8b
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-03-30 22:50:29 +0530

    Input form frontend
    Created Input form for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 //components
 import Header from "../src/components/Header";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
     <div>
       <Header />
+      <TodoForm />
     </div>
   );
 }
---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,11 @@
+import "./TodoForm.css";
+
+const TodoForm = () => {
+  return (
+    <form>
+      <input placeholder="  Enter new task into your to do list..."></input>
+    </form>
+  );
+};
+
+export default TodoForm;
