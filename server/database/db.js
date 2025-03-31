39017c628fa73bc31896db2aa8eb08bf54531916
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-04-01 00:39:27 +0530

    DB Connection Successful
    successfully connected with DB

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-export const connection = () => {
-  MONGODB_URL =
-    "mongodb+srv://<db_username>:<db_password>@mern-todo.vnltbyh.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Todo";
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const connection = () => {
+  const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-todo.vnltbyh.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Todo`;
 
   mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
+
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected succesfully");
+  });
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
+  mongoose.connection.on("error", (error) => {
+    console.log("Error while connectiong with database ", error.message);
+  });
 };
+
+export default connection;
