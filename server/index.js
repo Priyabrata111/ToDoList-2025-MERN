847fbfb3e57bb38edb763dd8bfe44316cf2fd0be
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-04-05 23:57:38 +0530

    Connected backend to frontend - part 1

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import connection from "./database/db.js";
+import cors from "cors";
+import Routes from "./routes/route.js";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", Routes);
 
 const PORT = 8000;
 
