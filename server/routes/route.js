136f493e4773f6fbe363c65126e2aed5432db825
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-04-08 01:45:20 +0530

    modifying DB & before that verifying frontend data

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { addTodo } from "../controller/todo-controller.js";
 
 const route = express.Router();
 
-route.post("/todos", (request, response) => {
-  console.log(request.body);
-});
+route.post("/todos", addTodo);
 
 export default route;
