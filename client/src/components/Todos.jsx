e88450a38a61975e09644050e5e9d16574582f80
Priyabrata Mondal <priyabratamondal622@gmail.com>
2025-04-22 23:19:04 +0530

    Update Todos.jsx

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,4 +1,4 @@
-import { getAllTodos } from "../redux/actions/index";
+import { getAllTodos } from "../redux/actions/index.js";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Todo from "./Todo";
